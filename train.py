552e3adeed8653961c0b3fe784a4b416715d3f43
Gautam J <gautam.jayapal@gmail.com>
2020-08-08 15:44:26 +0530

    add history plots

--- train.py
+++ train.py
@@ -4,16 +4,19 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, classification_report
 from tensorflow.keras import Sequential
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from tensorflow.keras.utils import plot_model
 from tensorflow.keras.callbacks import EarlyStopping
+
+plt.style.use('seaborn')
 
 trainDir = 'data/train'
 testDir = 'data/test'
 batchSize = 32
-nEpochs = 1
+nEpochs = 5
 imgHeight = 48
 imgWidth = 48
 
@@ -23,12 +26,10 @@ print(f'{nClasses = }')
 print(f'{numberOfTrainingImages = }')
 
 classWeights = {}
-classNames = []
 for sno, class_ in enumerate(os.listdir(trainDir)):
     bincount = len(list(os.listdir(os.path.join(trainDir, class_))))
     print(f'{class_ = }, {bincount = }')
     classWeights[sno] = numberOfTrainingImages / (nClasses * bincount)
-    classNames.append(class_)
 
 print(f'{classWeights = }')
 
@@ -75,11 +76,14 @@ testGenerator = testDatagen.flow_from_directory(
     batch_size=batchSize)
 
 model = Sequential()
-model.add(Conv2D(64, 2, input_shape=(imgWidth, imgHeight, 1), activation='relu'))
+model.add(Conv2D(32, 3, input_shape=(imgWidth, imgHeight, 1), activation='relu', padding='same'))
 model.add(MaxPooling2D(2))
-model.add(Conv2D(64, 2, activation='relu'))
+model.add(Conv2D(64, 3, activation='relu', padding='same'))
+model.add(MaxPooling2D(2))
+model.add(Conv2D(128, 2, activation='relu', padding='same'))
 model.add(MaxPooling2D(2))
 model.add(Flatten())
+model.add(Dense(512, activation='relu'))
 model.add(Dense(128, activation='relu'))
 model.add(Dense(nClasses, activation='softmax'))
 
@@ -88,6 +92,7 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy', 'AUC'])
 
 model.summary()
+plot_model(model, to_file='models/baseline_model.png', show_shapes=True, dpi=200)
 
 earlyStopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
 stepsPerEpoch = trainGenerator.samples // trainGenerator.batch_size
@@ -104,16 +109,60 @@ model.save('models/baseline.model')
 
 testMetrics = model.evaluate(testGenerator)
 print('\n\tTest Metrics:')
-print(testMetrics)
+print('Test Loss =', testMetrics[0])
+print('Test Accuracy =', testMetrics[1])
+print('Test AUC =', testMetrics[2])
 
 y_pred = np.argmax(model.predict(testGenerator), axis=-1)
-y_test = np.array([labels for _, labels in testGenerator])
+y_test = testGenerator.classes
+classLabels = list(testGenerator.class_indices.keys())
+
+report = classification_report(y_test, y_pred, target_names=classLabels, output_dict=True)
+print('\n\tClassification Report')
+print(report)
 
 matrix = confusion_matrix(y_test, y_pred)
 matrix = matrix / matrix.astype(np.float).sum(axis=0)
-df = pd.DataFrame(matrix, index=classNames, columns=classNames)
+print('\n\tConfusion Matrix')
+print(matrix)
 
-fig = plt.figure(figsize=(12, 12))
+df = pd.DataFrame(report)
+cr = sns.heatmap(df, annot=True, cmap='coolwarm')
+cr.yaxis.set_ticklabels(cr.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=10)
+cr.xaxis.set_ticklabels(cr.xaxis.get_ticklabels(), rotation=45, ha='right', fontsize=10)
+plt.savefig('models/baseline_cr.png')
+plt.show()
+
+df = pd.DataFrame(matrix, index=classLabels, columns=classLabels)
 hm = sns.heatmap(df, annot=True, cmap='coolwarm')
 hm.yaxis.set_ticklabels(hm.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=10)
 hm.xaxis.set_ticklabels(hm.xaxis.get_ticklabels(), rotation=45, ha='right', fontsize=10)
+plt.savefig('models/baseline_cm.png')
+plt.show()
+
+plt.subplot(131)
+plt.plot(history.history['loss'], label='Train Loss')
+plt.plot(history.history['val_loss'], label='Val Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('Loss')
+plt.legend()
+
+plt.subplot(132)
+plt.plot(history.history['accuracy'], label='Train Accuracy')
+plt.plot(history.history['val_accuracy'], label='Val Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title('Accuracy')
+plt.legend()
+
+plt.subplot(133)
+plt.plot(history.history['auc'], label='Train AUC')
+plt.plot(history.history['val_auc'], label='Val AUC')
+plt.xlabel('Epoch')
+plt.ylabel('AUC')
+plt.title('Area Under ROC')
+plt.legend()
+
+plt.savefig('models/baseline_history.png')
+plt.show()
