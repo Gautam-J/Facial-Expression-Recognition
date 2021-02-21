e17070d29e11110990ca344079f7b1824abe1db9
Gautam-J <gautam.jayapal@gmail.com>
2021-02-21 19:56:33 +0530

    update utils add docs

--- utils.py
+++ utils.py
@@ -4,6 +4,12 @@ import pathlib
 
 
 def createBaseDir():
+    """Creates a directory for storing all the models.
+
+    Returns:
+        str: Path to the directory created.
+    """
+
     baseDir = f'models/model_{int(time.time())}'
 
     if not os.path.exists(baseDir):
@@ -21,6 +27,15 @@ def getNumberOfTrainingImages(path_to_train_dir):
 
 
 def getClassWeights(path_to_train_dir):
+    """Get class weights for the training dataset.
+
+    Args:
+        path_to_train_dir (str): Path where the train images are stored.
+
+    Returns:
+        dict: Contains class weights for each class.
+    """
+
     nClasses = getNumberOfClasses(path_to_train_dir)
     numberOfTrainingImages = getNumberOfTrainingImages(path_to_train_dir)
     classList = sorted(os.listdir(path_to_train_dir))
